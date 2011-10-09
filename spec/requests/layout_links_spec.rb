bfa529c4699fb1191a98669798a38c07294b12e5
Linda Ruiz <skygal.linda@gmail.com>
2011-10-09 11:48:07 -0500

    Finished lab5.1

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -34,7 +34,7 @@ describe "LayoutLinks" do
     click_link "Home"
     response.should have_selector('title', :content => "Home")
     click_link "Sign up now!"
-    response.should have_selector('title', :content => "Sign in")
+    response.should have_selector('title', :content => "Sign up")
   end
 
 end
